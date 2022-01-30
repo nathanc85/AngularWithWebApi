2dcfc5a9b20779e5a175220ad6b277f99a0faf67
Nathan C <cnlnat@yahoo.com>
2022-01-30 00:31:17 -0500

    Adding the api

---
+++ api/DotNetWebApi/DotNetWebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DotNetWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ api/DotNetWebApi/DotNetWebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DotNetWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
